2d934b569613027dc63d8a74598a603adc8063d7
andy0303-cn <82488935@qq.com>
2025-02-02 05:03:31 +0800

    Restore original prompts and save optimized version

---
+++ core/prompts_new.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+current_year = datetime.now().year
+chinese_zodiac_year = "蛇"  # 2025年是蛇年
+
+FORTUNE_KNOWLEDGE_BASE = f"""
+# 2025年总体运势特点
+- 2025年是乙巳年，属蛇年，五行属木
+...
+"""
+
+# ... (保存其他优化的提示词内容) 
