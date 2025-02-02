7767fe0bc1777f5ebe6e268f1a8533c9c1664de6
andy0303-cn <82488935@qq.com>
2025-02-02 13:53:48 +0800

    Add debug information to Flask app

--- api/index.py
+++ api/index.py
@@ -1,7 +1,25 @@
+import sys
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World' 
+    # 打印环境信息
+    python_version = sys.version
+    python_path = sys.path
+    current_dir = os.getcwd()
+    env_vars = dict(os.environ)
+    
+    return f"""
+    Python Version: {python_version}
+    Python Path: {python_path}
+    Current Directory: {current_dir}
+    Environment Variables: {env_vars}
+    """
+
+# 添加错误处理
+@app.errorhandler(500)
+def handle_500(e):
+    return str(e), 500 
